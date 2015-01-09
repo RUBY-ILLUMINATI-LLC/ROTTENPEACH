afd432b38d2b2a2c31f7bc71cf0b645b7d669d2f
Sean Coyle <scoyle98@gmail.com>
2015-01-09 16:28:12 -0500

    round one added

--- gameOn.rb
+++ gameOn.rb
@@ -2,3 +2,20 @@ puts "Welcome to ROTTENPEACH! Brought to you by Ruby Illuminati LLC. Please ente
 name = gets.chomp
 
 health_points = 10
+
+bad_guy_health = 8
+
+puts "the evil slumlord CRAWFORD wants to fight you"
+puts "fight or run chump TYPE (fight/run)"
+choice = gets.chomp
+if choice == "fight"
+   failure = Random.rand(1...5)
+  if failure == 1
+     puts "missed bro"
+  else failure != 1
+     loss = Random.rand(2...4)
+     bad_guy_health = bad_guy_health - loss
+     puts "Your attack landed. Your enemy lost #{loss} points"
+     puts "Your enemy now has #{bad_guy_health} health points"
+  end
+end
